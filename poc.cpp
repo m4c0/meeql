2b32a7188e88f2288894489a35dab5744fe52ba7
Eduardo Costa <m4c0@users.noreply.github.com>
2024-10-29 11:57:15 +0000

    recursive list of poms in m2/repo

--- poc.cpp
+++ poc.cpp
@@ -1,6 +1,10 @@
 #pragma leco tool
+#include <stdlib.h>
 
 import cavan;
+import jute;
+import pprent;
+import silog;
 import tora;
 
 void init(tora::db & db) {
@@ -19,10 +23,28 @@ void init(tora::db & db) {
   )");
 }
 
-int main(int argc, char ** argv) try {
-  tora::db db { ":memory:" };
+void process_dep(tora::db & db, jute::view repo_dir, jute::view path) {
+  silog::trace(path);
+}
 
+void recurse(tora::db & db, jute::view repo_dir, jute::view path) {
+  auto full_path = (repo_dir + path).cstr();
+  for (auto f : pprent::list(full_path.begin())) {
+    auto ff = jute::view::unsafe(f);
+    if (f[0] == '.') continue;
+    if (ff == "_remote.repositories") process_dep(db, repo_dir, path.subview(1).after);
+    else {
+      auto dir = (path + "/" + jute::view::unsafe(f)).cstr();
+      recurse(db, repo_dir, dir);
+    }
+  }
+}
+
+int main(int argc, char ** argv) try {
+  const auto repo_dir = (jute::view::unsafe(getenv("HOME")) + "/.m2/repository/").cstr();
+  tora::db db { ":memory:" };
   init(db);
+  recurse(db, repo_dir, "");
 } catch (...) {
   return 1;
 }
