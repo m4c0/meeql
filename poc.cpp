f66ad20f89403cabe480d757a6a1475ac7955b0b
Eduardo Costa <m4c0@users.noreply.github.com>
2024-10-29 14:04:18 +0000

    refactor + preparing for deps

--- poc.cpp
+++ poc.cpp
@@ -10,6 +10,9 @@ import tora;
 
 static const auto repo_dir = (jute::view::unsafe(getenv("HOME")) + "/.m2/repository").cstr();
 
+static tora::stmt g_pom_stmt;
+static tora::stmt g_dep_stmt;
+
 void setup_schema(tora::db & db) {
   db.exec(R"(
     CREATE TABLE pom (
@@ -26,23 +29,56 @@ void setup_schema(tora::db & db) {
       p_version     TEXT,
       parent        INTEGER REFERENCES pom(id)
     ) STRICT;
+
+    CREATE TABLE deps (
+      id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+
+      owner_pom INTEGER NOT NULL REFERENCES pom(id),
+
+      group_id       TEXT NOT NULL,
+      artefact_id    TEXT NOT NULL,
+      version        TEXT NOT NULL,
+      type           TEXT NOT NULL,
+      scope          TEXT NOT NULL,
+      classification TEXT NOT NULL,
+      optional       INTEGER NOT NULL
+    ) STRICT;
   )");
 }
 
-void persist_pom(tora::stmt & stmt, cavan::pom * pom, auto ftime) {
-  stmt.reset();
-  stmt.bind(1, pom->filename);
-  stmt.bind64(2, ftime);
-  stmt.bind(3, pom->grp);
-  stmt.bind(4, pom->art);
-  stmt.bind(5, pom->ver);
-  stmt.bind(6, pom->parent.grp);
-  stmt.bind(7, pom->parent.art);
-  stmt.bind(8, pom->parent.ver);
-  stmt.step();
+void prepare_statements(tora::db & db) {
+  g_pom_stmt = db.prepare(R"(
+    INSERT INTO pom (
+      filename, fmod,
+      group_id, artefact_id, version,
+      p_group_id, p_artefact_id, p_version
+    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+  )");
+  g_dep_stmt = db.prepare(R"(
+    INSERT INTO dep (
+    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+  )");
 }
 
-void process_dep(tora::stmt & stmt, jute::view path) {
+void persist_pom(cavan::pom * pom, auto ftime) {
+  g_pom_stmt.reset();
+  g_pom_stmt.bind(1, pom->filename);
+  g_pom_stmt.bind64(2, ftime);
+  g_pom_stmt.bind(3, pom->grp);
+  g_pom_stmt.bind(4, pom->art);
+  g_pom_stmt.bind(5, pom->ver);
+  g_pom_stmt.bind(6, pom->parent.grp);
+  g_pom_stmt.bind(7, pom->parent.art);
+  g_pom_stmt.bind(8, pom->parent.ver);
+  g_pom_stmt.step();
+
+  // for (auto &[d, _] : pom->deps) {}
+  // for (auto &[d, _] : pom->deps_mgmt) {}
+  // for (auto &[d, _] : pom->props) {}
+  // for (auto d : pom->modules) {}
+}
+
+void process_dep(jute::view path) {
   auto [l_ver, ver] = path.rsplit('/');
   auto [l_art, art] = l_ver.rsplit('/');
 
@@ -52,31 +88,24 @@ void process_dep(tora::stmt & stmt, jute::view path) {
   if (ftime == 0) return;
 
   auto pom = cavan::read_pom(fname);
-  persist_pom(stmt, pom, ftime);
+  persist_pom(pom, ftime);
 }
 
-void recurse(tora::stmt & stmt, jute::view path) {
+void recurse(jute::view path) {
   auto full_path = (repo_dir + path).cstr();
   auto marker = repo_dir + path + "/_remote.repositories";
   if (mtime::of(marker.cstr().begin())) {
-    process_dep(stmt, path);
+    process_dep(path);
     return;
   }
   for (auto f : pprent::list(full_path.begin())) {
     if (f[0] == '.') continue;
     auto dir = (path + "/" + jute::view::unsafe(f)).cstr();
-    recurse(stmt, dir);
+    recurse(dir);
   }
 }
 void import_local_repo(tora::db & db) {
-  auto stmt = db.prepare(R"(
-    INSERT INTO pom (
-      filename, fmod,
-      group_id, artefact_id, version,
-      p_group_id, p_artefact_id, p_version
-    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-  )");
-  recurse(stmt, "");
+  recurse("");
 
   // Update parent keys with their IDs
   db.exec(R"(
@@ -90,8 +119,9 @@ void import_local_repo(tora::db & db) {
 }
 
 int main(int argc, char ** argv) try {
-  tora::db db { ":memory:" };
+  tora::db db { "out/test.sql" };
   setup_schema(db);
+  prepare_statements(db);
   import_local_repo(db);
 } catch (...) {
   return 1;
