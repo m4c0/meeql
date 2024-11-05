f2c83ee6c6980bb122cfe0c15e5add6245142444
Eduardo Costa <m4c0@users.noreply.github.com>
2024-11-05 11:50:02 +0000

    tweaking loader

--- load.cpp
+++ load.cpp
@@ -73,33 +73,6 @@ void setup_schema(tora::db & db) {
       group_id    TEXT NOT NULL,
       artefact_id TEXT NOT NULL
     ) STRICT;
-
-    CREATE VIEW f_pom_tree AS
-    WITH RECURSIVE
-      pom_chain(id, root, depth) AS (
-        SELECT id, id, 0
-        FROM pom
-        UNION ALL
-        SELECT pom.parent, pom_chain.root, pom_chain.depth + 1
-        FROM pom
-        JOIN pom_chain ON pom_chain.id = pom.id
-        WHERE pom.parent IS NOT NULL
-      )
-    SELECT * FROM pom_chain;
-
-    CREATE VIEW f_prop AS
-    SELECT t.root, prop.*, t.depth
-    FROM prop
-    JOIN f_pom_tree t ON t.id = prop.owner_pom
-    GROUP BY t.root, prop.key
-    HAVING depth = MIN(depth);
-
-    CREATE VIEW f_dep AS
-    SELECT t.root, dep.*, depth
-    FROM dep
-    JOIN f_pom_tree t ON t.id = dep.owner_pom
-    GROUP BY t.root, dep.dep_mgmt, dep.group_id, dep.artefact_id
-    HAVING depth = MIN(depth);
   )");
 }
 
@@ -217,9 +190,12 @@ void recurse(jute::view path) {
   }
 }
 void import_local_repo(tora::db & db) {
+  silog::log(silog::info, "parsing and importing local repo");
   recurse("");
+}
 
-  // Update parent keys with their IDs
+void update_parent_keys(tora::db & db) {
+  silog::log(silog::info, "updating parent keys");
   db.exec(R"(
     UPDATE OR IGNORE pom AS p
     SET parent = par.id
@@ -227,6 +203,38 @@ void import_local_repo(tora::db & db) {
       WHERE par.group_id    = p.p_group_id
         AND par.artefact_id = p.p_artefact_id
         AND par.version     = p.p_version
+  )");
+}
+
+void setup_aux_tables(tora::db & db) {
+  silog::log(silog::info, "setting up auxiliary tables");
+  db.exec(R"(
+    CREATE TABLE f_pom_tree AS
+    WITH RECURSIVE
+      pom_chain(id, root, depth) AS (
+        SELECT id, id, 0
+        FROM pom
+        UNION ALL
+        SELECT pom.parent, pom_chain.root, pom_chain.depth + 1
+        FROM pom
+        JOIN pom_chain ON pom_chain.id = pom.id
+        WHERE pom.parent IS NOT NULL
+      )
+    SELECT * FROM pom_chain;
+
+    CREATE VIEW f_prop AS
+    SELECT t.root, prop.*, t.depth
+    FROM prop
+    JOIN f_pom_tree t ON t.id = prop.owner_pom
+    GROUP BY t.root, prop.key
+    HAVING depth = MIN(depth);
+
+    CREATE VIEW f_dep AS
+    SELECT t.root, dep.*, depth
+    FROM dep
+    JOIN f_pom_tree t ON t.id = dep.owner_pom
+    GROUP BY t.root, dep.dep_mgmt, dep.group_id, dep.artefact_id
+    HAVING depth = MIN(depth);
   )");
 }
 
@@ -262,6 +270,8 @@ int main(int argc, char ** argv) try {
   setup_schema(db);
   prepare_statements(db);
   import_local_repo(db);
+  update_parent_keys(db);
+  setup_aux_tables(db);
   dump_stats(db);
 
   silog::log(silog::info, "All of that done in %dms", t.millis());
