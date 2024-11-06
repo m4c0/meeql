7ed6a0cd008ff2176b1abdb83e9281584241dfd4
Eduardo Costa <m4c0@users.noreply.github.com>
2024-11-06 11:54:59 +0000

    some work on dep traversal

--- poc.cpp
+++ poc.cpp
@@ -10,18 +10,10 @@ static auto v(const unsigned char * n) {
   return jute::view::unsafe(reinterpret_cast<const char *>(n));
 }
 
-int main(int argc, char ** argv) {
-  if (argc < 3) {
-    silog::log(silog::error, "requires group/artefact/version");
-    return 1;
-  }
-
+static void work(jute::view grp, jute::view art, jute::view ver, jute::view scope) {
   auto db = meeql::db();
 
   silog::trace(1);
-  auto grp = jute::view::unsafe(argv[1]);
-  auto art = jute::view::unsafe(argv[2]);
-  auto ver = jute::view::unsafe(argv[3]);
   meeql::eff(db, grp, art, ver, 0);
 
   silog::trace(2);
@@ -40,6 +32,7 @@ int main(int argc, char ** argv) {
   silog::trace(5);
 
   stmt = db.prepare(R"(
+    INSERT OR IGNORE INTO r_deps (group_id, artefact_id, version)
     SELECT d.group_id, d.artefact_id
          , COALESCE(d.version, dm.version)
     FROM eff_dep AS d
@@ -48,16 +41,65 @@ int main(int argc, char ** argv) {
      AND d.artefact_id = dm.artefact_id
      AND dm.dep_mgmt = 1
     WHERE d.dep_mgmt = 0
-      AND COALESCE(d.scope, 'compile') IN ('test', 'compile')
+      AND COALESCE(d.scope, 'compile') IN (?, 'compile')
       AND COALESCE(d.type, 'jar') = 'jar'
       AND NOT d.optional
     GROUP BY d.group_id, d.artefact_id
     HAVING d.depth = MIN(d.depth)
   )");
-  while (stmt.step()) {
-    putfn("%s:%s:%s",
-        stmt.column_text(0),
-        stmt.column_text(1),
-        stmt.column_text(2));
+  stmt.bind(1, scope);
+  stmt.step();
+
+  silog::trace(6);
+}
+
+int main(int argc, char ** argv) {
+  if (argc < 3) {
+    silog::log(silog::error, "requires group/artefact/version");
+    return 1;
+  }
+
+  meeql::db().exec(R"(
+    DROP TABLE IF EXISTS r_deps;
+    CREATE TABLE r_deps (
+      id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+      group_id    TEXT NOT NULL,
+      artefact_id TEXT NOT NULL,
+      version     TEXT NOT NULL,
+      worked      INT NOT NULL DEFAULT 0,
+      UNIQUE (group_id, artefact_id)
+    );
+  )");
+
+  work(
+    jute::view::unsafe(argv[1]),
+    jute::view::unsafe(argv[2]),
+    jute::view::unsafe(argv[3]),
+    "test"
+  );
+
+  while (true) {
+    jute::heap grp, art, ver;
+    {
+      auto db = meeql::db();
+      auto stmt = db.prepare(R"(
+        UPDATE r_deps
+        SET worked = 1
+        WHERE id IN (
+          SELECT id
+          FROM r_deps
+          WHERE worked = 0
+          ORDER BY id ASC
+          LIMIT 1
+        )
+        RETURNING group_id, artefact_id, version
+      )");
+      if (!stmt.step()) break;
+      grp = v(stmt.column_text(0));
+      art = v(stmt.column_text(1));
+      ver = v(stmt.column_text(2));
+      putfn("%s:%s:%s", grp.begin(), art.begin(), ver.begin());
+    }
+    work(*grp, *art, *ver, "compile");
   }
 }
