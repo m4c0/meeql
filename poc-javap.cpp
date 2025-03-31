1afa3f7d6626cc7263882b87ef4829e1d0187619
Eduardo Costa <m4c0@users.noreply.github.com>
2025-03-31 18:24:29 +0100

    javap: loading all classes into db

--- poc-javap.cpp
+++ poc-javap.cpp
@@ -2,6 +2,8 @@
 
 import jute;
 import meeql;
+import mtime;
+import popen;
 import silog;
 import tora;
 
@@ -28,10 +30,11 @@ auto curry(auto fn, auto param) {
 
 static void add_jar(tora::stmt * stmt, jute::view pom_path) {
   auto jar_path = jute::heap { pom_path.rsplit('.').before } + ".jar";
+  if (!mtime::of((*jar_path).cstr().begin())) return;
+
   stmt->reset();
   stmt->bind(1, *jar_path);
   stmt->step();
-  silog::trace(jar_path);
 }
 
 static void import_local_repo(tora::db & db) {
@@ -43,11 +46,43 @@ static void import_local_repo(tora::db & db) {
   meeql::recurse_repo_dir(curry(add_jar, &stmt));
 }
 
+static auto d(jute::view str) { return str.cstr(); }
+
+static void import_classes(tora::db & db) {
+  silog::log(silog::info, "indexing classes");
+
+  auto i_stmt = db.prepare(R"(
+    INSERT INTO class (name, jar)
+    VALUES (?, ?)
+  )");
+
+  auto stmt = db.prepare("SELECT id, name FROM jar");
+  while (stmt.step()) {
+    auto id   = stmt.column_int(0);
+    auto name = stmt.column_view(1).cstr();
+
+    auto unzip = d("unzip");
+    auto qq    = d("-qq");
+    auto l     = d("-l");
+    auto cls   = d("*.class");
+    char * args[] { unzip.begin(), qq.begin(), l.begin(), name.begin(), cls.begin(), 0 };
+    p::proc p { args };
+    while (p.gets()) {
+      auto name = jute::view::unsafe(p.last_line_read()).rsplit(' ').after.split('\n').before;
+      i_stmt.reset();
+      i_stmt.bind(1, name);
+      i_stmt.bind(2, id);
+      i_stmt.step();
+    }
+  }
+}
+
 int main() {
   tora::db db { ":memory:" };
 
   setup_schema(db);
   import_local_repo(db);
+  import_classes(db);
 
   silog::log(silog::info, "done");
 }
