890677c041def4e6725ac9621157ebc6c547b4bf
Eduardo Costa <m4c0@users.noreply.github.com>
2025-04-25 11:51:06 +0100

    depdb: encapsulate remaining queries

--- poc-depdb.cpp
+++ poc-depdb.cpp
@@ -49,6 +49,7 @@ static auto curry(auto fn, auto param) {
   return db;
 }
 
+struct version_not_found {};
 class db {
   tora::db m_db = init_db();
 
@@ -71,10 +72,24 @@ class db {
     RETURNING id
   )");
 
+  tora::stmt m_ins_dm_stmt = m_db.prepare(R"(
+    INSERT INTO dep_mgmt (from_ver, to_ver) VALUES (?, ?)
+  )");
+
+  tora::stmt m_find_ver_stmt = m_db.prepare(R"(
+    SELECT ver.id
+    FROM ver
+    JOIN art ON art.id = ver.art
+    JOIN grp ON grp.id = art.grp
+    WHERE grp.name = ?
+      AND art.name = ?
+      AND ver.name = ?
+  )");
+
 public:
   [[nodiscard]] constexpr auto * handle() { return &m_db; }
 
-  unsigned ensure(jute::view grp, jute::view art, jute::view ver, jute::view pom) {
+  unsigned insert(jute::view grp, jute::view art, jute::view ver, jute::view pom) {
     m_ins_grp_stmt.reset();
     m_ins_grp_stmt.bind(1, grp);
     m_ins_grp_stmt.step();
@@ -92,6 +107,24 @@ public:
 
     return m_ins_ver_stmt.column_int(0);
   }
+
+  void add_dep_mgmt(unsigned from, unsigned to) {
+    m_ins_dm_stmt.reset();
+    m_ins_dm_stmt.bind(1, from);
+    m_ins_dm_stmt.bind(2, to);
+    m_ins_dm_stmt.step();
+  }
+
+  [[nodiscard]] unsigned find_ver(jute::view grp, jute::view art, jute::view ver) {
+    m_find_ver_stmt.reset();
+    m_find_ver_stmt.bind(1, grp);
+    m_find_ver_stmt.bind(2, art);
+    m_find_ver_stmt.bind(3, ver);
+    if (!m_find_ver_stmt.step()) throw version_not_found {};
+    auto id = m_find_ver_stmt.column_int(0); 
+    if (m_find_ver_stmt.step()) die("duplicate version found");
+    return id;
+  }
 };
 
 struct load_stuff {
@@ -99,8 +132,6 @@ struct load_stuff {
 };
 struct dep_stuff {
   db * db;
-  tora::stmt ver_stmt;
-  tora::stmt dm_stmt;
 };
 
 static void load_pom(load_stuff * ls, jute::view pom_path) {
@@ -112,22 +143,9 @@ static void load_pom(load_stuff * ls, jute::view pom_path) {
   auto grp = r2.cstr();
   for (auto & c : grp) if (c == '/') c = '.';
 
-  ls->db->ensure(grp, art, ver, pom_path);
+  ls->db->insert(grp, art, ver, pom_path);
 }
 
-struct version_not_found {};
-static unsigned find_ver(dep_stuff * ds, jute::view grp, jute::view art, jute::view ver) {
-  ds->ver_stmt.reset();
-  ds->ver_stmt.bind(1, grp);
-  ds->ver_stmt.bind(2, art);
-  ds->ver_stmt.bind(3, ver);
-  if (!ds->ver_stmt.step()) throw version_not_found {};
-
-  unsigned id = ds->ver_stmt.column_int(0);
-  if (ds->ver_stmt.step()) die("duplicate version found");
-
-  return id;
-}
 static void load_deps(dep_stuff * ds, jute::view pom_path) {
   static hai::varray<unsigned> buffer { 1024 };
   buffer.truncate(0);
@@ -136,20 +154,15 @@ static void load_deps(dep_stuff * ds, jute::view pom_path) {
   cavan::merge_props(pom);
 
   try {
-    auto from = find_ver(ds, pom->grp, pom->art, pom->ver);
+    auto from = ds->db->find_ver(pom->grp, pom->art, pom->ver);
     for (auto &[d, _]: pom->deps_mgmt) {
       auto grp = cavan::apply_props(pom, d.grp);
       auto ver = cavan::apply_props(pom, d.ver);
-      auto to = find_ver(ds, *grp, d.art, *ver);
+      auto to = ds->db->find_ver(*grp, d.art, *ver);
       buffer.push_back_doubling(to);
     }
 
-    for (auto to : buffer) {
-      ds->dm_stmt.bind(1, from);
-      ds->dm_stmt.bind(2, to);
-      ds->dm_stmt.step();
-      ds->dm_stmt.reset();
-    }
+    for (auto to : buffer) ds->db->add_dep_mgmt(from, to);
   } catch (version_not_found) {
   }
 }
@@ -163,18 +176,6 @@ int main(int argc, char ** argv) try {
   meeql::recurse_repo_dir(curry(load_pom, &ls));
   dep_stuff ds {
     .db = &db,
-    .ver_stmt = db.handle()->prepare(R"(
-      SELECT ver.id
-      FROM ver
-      JOIN art ON art.id = ver.art
-      JOIN grp ON grp.id = art.grp
-      WHERE grp.name = ?
-        AND art.name = ?
-        AND ver.name = ?
-    )"),
-    .dm_stmt = db.handle()->prepare(R"(
-      INSERT INTO dep_mgmt (from_ver, to_ver) VALUES (?, ?)
-    )"),
   };
   meeql::recurse_repo_dir(curry(load_deps, &ds));
 
