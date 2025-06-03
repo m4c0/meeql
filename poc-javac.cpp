6ea119e9fc44b3031af80825fee6286fa603e0cf
Eduardo Costa <m4c0@users.noreply.github.com>
2025-06-03 11:27:33 +0100

    javac: caching results

--- poc-javac.cpp
+++ poc-javac.cpp
@@ -3,7 +3,46 @@
 import cavan;
 import jojo;
 import jute;
+import hai;
 import print;
+import tora;
+
+[[nodiscard]] static auto resolve_deps(jute::view fname) {
+  tora::db db { "out/poc-javac.sqlite" }; // TODO: move to .m2
+  db.exec(R"(
+    CREATE TABLE IF NOT EXISTS rdep (
+      s_pom TEXT NOT NULL,
+      t_jar TEXT NOT NULL
+    ) STRICT
+  )");
+
+  auto stmt = db.prepare("SELECT t_jar FROM rdep WHERE s_pom = ?");
+  stmt.bind(1, fname);
+
+  hai::chain<hai::cstr> res { 1000 };
+  while (stmt.step()) {
+    res.push_back(stmt.column_view(0).cstr());
+  }
+  if (res.size()) return res;
+
+  cavan::file_reader = jojo::read_cstr;
+  auto pom = cavan::read_pom(fname);
+  auto deps = cavan::resolve_transitive_deps(pom);
+
+  stmt = db.prepare("INSERT INTO rdep (s_pom, t_jar) VALUES (?, ?)");
+  for (auto i = 0; i < deps.size(); i++) {
+    auto p = deps.seek(i);
+    auto jar = cavan::path_of(p->grp, p->art, p->ver, "jar");
+
+    stmt.reset();
+    stmt.bind(1, fname);
+    stmt.bind(2, jar);
+    stmt.step();
+
+    res.push_back(traits::move(jar));
+  }
+  return res;
+}
 
 int main(int argc, char ** argv) try {
   const auto shift = [&] { return jute::view::unsafe(argc == 1 ? "" : (--argc, *++argv)); };
@@ -11,12 +50,8 @@ int main(int argc, char ** argv) try {
   auto file = shift();
   if (file == "") die("missing file");
 
-  cavan::file_reader = jojo::read_cstr;
-
-  auto pom = cavan::read_pom(file);
-
-  for (auto p : cavan::resolve_transitive_deps(pom)) {
-    putln(cavan::path_of(p->grp, p->art, p->ver, "jar"));
+  for (auto & p : resolve_deps(file)) {
+    putln(p);
   }
 } catch (...) {
   return 3;
