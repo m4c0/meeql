ca24333748a2ef5a3ab966e7eac577fd6ee974f9
Eduardo Costa <m4c0@users.noreply.github.com>
2025-08-14 16:50:02 +0100

    brute: basic dep resolution

--- brute.cpp
+++ brute.cpp
@@ -1,9 +1,16 @@
 #pragma leco tool
 
+import cavan;
+import jojo;
 import jute;
 import meeql;
+import mtime;
+import pprent;
+import popen;
 import print;
 import tora;
+
+using namespace jute::literals;
 
 static void help() {
   errln(R"(
@@ -13,7 +20,59 @@ Available commands (in alphabetical order):
 )");
 }
 
-static void init(tora::db & db) {
+static void load(tora::db & db, jute::view path) {
+  if (!mtime::of((path + "/pom.xml").cstr().begin())) return;
+  for (auto d : pprent::list(path.cstr().begin())) {
+    if (d[0] == '.') continue;
+
+    auto dp = (path + "/" + jute::view::unsafe(d)).cstr();
+    load(db, dp);
+
+    auto deps = (dp + "/target/meeql-brute"_s).cstr();
+    if (!mtime::of(deps.begin())) continue;
+
+    auto src = jojo::read_cstr(deps);
+    jute::view rest = src;
+    while (rest.size()) {
+      auto [l, r] = rest.split('\n');
+      rest = r;
+      if (!l.starts_with("   ")) continue;
+
+      auto [grp, rg] = l.trim().split(':');
+      auto [art, ra] = rg.split(':');
+      auto [typ, rt] = ra.split(':');
+      auto [ver, sc] = rt.split(':');
+      auto jar = cavan::path_of(grp, art, ver, "jar");
+      putln(jar);
+    }
+  }
+}
+static void load(tora::db & db) {
+  db.exec(R"(
+    CREATE TABLE IF NOT EXISTS dep (
+      id    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+      path  TEXT NOT NULL UNIQUE,
+      grp   TEXT NOT NULL,
+      art   TEXT NOT NULL,
+      ver   TEXT NOT NULL,
+      UNIQUE (grp, art)
+    ) STRICT;
+  )");
+
+  p::proc p {
+    "mvn",
+    "dependency:collect",
+    "-DoutputFile=target/meeql-brute",
+  };
+  bool erred = false;
+  while (p.gets()) {}
+  while (p.gets_err()) {
+    erred = true;
+    putln(p.last_line_read());
+  }
+  if (erred) die("mvn emitted some error");
+
+  load(db, ".");
 }
 
 int main(int argc, char ** argv) {
@@ -24,5 +83,5 @@ int main(int argc, char ** argv) {
 
   auto cmd = shift();
        if (cmd == "")     help();
-  else if (cmd == "init") init(db);
+  else if (cmd == "load") load(db);
 }
