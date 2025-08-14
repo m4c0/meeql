3854c81fbe388e7da658fc8e79085388b34cddbb
Eduardo Costa <m4c0@users.noreply.github.com>
2025-08-14 17:12:24 +0100

    brute: javac

--- brute.cpp
+++ brute.cpp
@@ -3,11 +3,13 @@
 import cavan;
 import jojo;
 import jute;
+import hai;
 import meeql;
 import mtime;
 import pprent;
 import popen;
 import print;
+import sysstd;
 import tora;
 
 using namespace jute::literals;
@@ -15,8 +17,9 @@ using namespace jute::literals;
 static void help() {
   errln(R"(
 Available commands (in alphabetical order):
-  * help - This command, list all commands
-  * load - Loads the current project dependencies into the database.
+  * javac - Runs javac with loaded DB as classpath
+  * help  - This command, list all commands
+  * load  - Loads the current project dependencies into the database.
 )");
 }
 
@@ -86,13 +89,51 @@ static void load(tora::db & db) {
   load(stmt, ".");
 }
 
-int main(int argc, char ** argv) {
+static int javac(tora::db & db, jute::view file) {
+  if (file == "") die("missing java source file");
+
+  hai::cstr rpath { 10240 };
+  sysstd::fullpath(file.cstr().begin(), rpath.begin(), rpath.size());
+
+  auto [root, test] = meeql::root_of(jute::view::unsafe(rpath.begin()));
+  auto tp = test ? jute::view{"test-"} : jute::view{};
+
+  auto gen_path = (root + "/target/generated-" + tp + "sources/annotations").cstr();
+  auto out_path = (root + "/target/" + tp + "classes").cstr();
+
+  jute::heap cp {};
+  auto stmt = db.prepare("SELECT path FROM dep");
+  while (stmt.step()) {
+    cp = cp + ":" + stmt.column_view(0);
+  }
+
+  const char * args[] {
+    "javac",
+    "-proc:full",
+    "-s",
+    gen_path.begin(),
+    "-d",
+    out_path.begin(),
+    "-source",
+    "21",
+    "-cp",
+    cp.begin(),
+    rpath.begin(),
+    0,
+  };
+  return sysstd::spawn("javac", args);
+}
+
+int main(int argc, char ** argv) try {
   const auto shift = [&] { return jute::view::unsafe(argc > 1 ? (--argc, *++argv) : ""); };
 
   auto file = meeql::m2_dir() + "/meeql-brute.sqlite\0";
   tora::db db { file.begin() };
 
   auto cmd = shift();
-       if (cmd == "")     help();
-  else if (cmd == "load") load(db);
+       if (cmd == "")      help();
+  else if (cmd == "javac") return javac(db, shift());
+  else if (cmd == "load")  load(db);
+} catch (...) {
+  return 1;
 }
